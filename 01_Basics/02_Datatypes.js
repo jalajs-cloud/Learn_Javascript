f572c7d3b7298f1bce0e5f73ee03482877530db7
jalaj kumar singh <59218593+jalajs-cloud@users.noreply.github.com>
2023-10-27 07:41:56 +0000

    02_Datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+console.log(3+3); //code readability should be high
+console.log("Jalaj");
+
+
+let name = "jalaj";
+let age = 18;
+let isLoggedIn = false;
+let state;
+
+
+//number
+//bigint
+//string =>""
+//boolean => true/false
+//null => standalone value {Representation of empty value}
+let state1 = null;
+//undefined => {value is not defined yet}
+//symbol => unique
+
+
+//object
+
+console.log(typeof "jalaj");
+console.log(typeof null);
+console.log(typeof undefined);
+
