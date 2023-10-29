733cf12b0ac48154f85b475a6a04ae0fc5a26d7a
jalaj kumar singh <59218593+jalajs-cloud@users.noreply.github.com>
2023-10-29 13:20:49 +0000

    Another add upto conversion topics

--- 01_Basics/03_ConversionOperations.js
+++ 01_Basics/03_ConversionOperations.js
@@ -1,27 +1,58 @@
-// let score = "33abc";
+// // let score = "33abc";
 
-// console.log(typeof score);
-// console.log(typeof(score));
+// // console.log(typeof score);
+// // console.log(typeof(score));
 
-// let valueInNumber = Number(score);
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
+// // let valueInNumber = Number(score);
+// // console.log(typeof valueInNumber);
+// // console.log(valueInNumber);
 
-// "33" => 33
-//"33abc" => NaN(Not a Number) but type of Nan is Number
-//true => 1 ; false => 0
+// // "33" => 33
+// //"33abc" => NaN(Not a Number) but type of Nan is Number
+// //true => 1 ; false => 0
 
-let isLoggedIn = "";
+// let isLoggedIn = "";
 
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
-console.log(typeof booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
 
-// 1=>true ; 0=> false
-// ""=> false
-//"jalaj" => true
+// // 1=>true ; 0=> false
+// // ""=> false
+// //"jalaj" => true
 
-let someNumber = 33;
-let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33;
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+// ****************************Opeartions**************************************
+
+let value = 3;
+let negativeValue = -value;
+//console.log(negativeValue);
+
+let str1 = "hello";
+
+let str2 = " jalaj";
+
+let str3 = str1 + str2;
+console.log(str3);
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(3+4*5%3);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2+2;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
