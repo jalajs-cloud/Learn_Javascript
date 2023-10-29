130c0a7932bfc16c51c2f0f9a2259ce7dd837594
jalaj kumar singh <59218593+jalajs-cloud@users.noreply.github.com>
2023-10-29 13:01:11 +0000

    03_ConversionOperations

---
+++ 01_Basics/03_ConversionOperations.js
@@ -0,0 +1,27 @@
+// let score = "33abc";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+//"33abc" => NaN(Not a Number) but type of Nan is Number
+//true => 1 ; false => 0
+
+let isLoggedIn = "";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+// 1=>true ; 0=> false
+// ""=> false
+//"jalaj" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
