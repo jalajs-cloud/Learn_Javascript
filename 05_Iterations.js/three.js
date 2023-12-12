764abe5f83835acf46ac34a882ccd30c3161f54b
jalaj kumar singh <59218593+jalajs-cloud@users.noreply.github.com>
2023-12-12 13:42:35 +0000

    for of for in

---
+++ 05_Iterations.js/three.js
@@ -0,0 +1,38 @@
+// for of
+
+// ["","",""]
+// [{},{},{}]
+
+const arr = [1,2,3,5,4];
+
+for (const num of arr) {
+    console.log(num);
+    
+}
+
+const greetings = "Hello world";
+
+for (const greet of greetings) {
+    console.log(`Each char is ${greet}`);
+}
+
+//Maps
+
+const map = new Map();
+map.set('IN',"India");
+map.set('USA','United States of America');
+map.set('FR','FRANCE');
+//console.log(map);
+
+for (const [key,value] of map) {
+    console.log(key , ':-', value);   
+}
+
+const myObject = {
+    'Game1' : 'NFS',
+    'Game2' : 'GTA'
+}
+
+for (const [key,value] of myObject) {
+    console.log(key , ':-', value);    
+}
