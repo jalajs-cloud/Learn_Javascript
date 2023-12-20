b7cafc02e7d5b12e0e87672f4a9c4a3a4a047afb
jalaj kumar singh <59218593+jalajs-cloud@users.noreply.github.com>
2023-12-20 08:02:56 +0000

    map,reduce

---
+++ 05_Iterations.js/map_function.js
@@ -0,0 +1,11 @@
+
+// MAP FUNCTION
+const myNumbers = [1,2,3,4,5,6,7,8,9,10];
+
+//const newNums = myNumbers.map((num)=> num + 10);
+
+const newNums = myNumbers.map((num) => num * 10).map((num) => num + 1).filter((num) => num >=40);  //chaining of methods
+
+console.log(newNums);
+
+
